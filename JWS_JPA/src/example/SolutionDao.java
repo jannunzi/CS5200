82e1626b2a2a2c21feed682835217f481d9a117d
jannunzi <jannunzi@gmail.com>
2014-04-20 05:58:52 -0400

    jQuery client to removeDirector() webservice end point

--- JWS_JPA/src/example/SolutionDao.java
+++ JWS_JPA/src/example/SolutionDao.java
@@ -4,8 +4,10 @@ import java.io.*;
 import java.util.*;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -39,7 +41,7 @@ public class SolutionDao {
 		return director;
 	}
 	
-	@GET
+	@DELETE
 	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Director> removeDirector(@PathParam("id") int directorId) {
