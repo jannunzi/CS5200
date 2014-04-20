f302a0d39384bafa7dd2e2c25014c7a6696081cf
jannunzi <jannunzi@gmail.com>
2014-04-20 03:47:44 -0400

    Removed all JWS work from here, will put in a separate project

--- XSLT_JPA/src/example/SolutionDao.java
+++ XSLT_JPA/src/example/SolutionDao.java
@@ -3,26 +3,17 @@ package example;
 import java.io.*;
 import java.util.*;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.xml.bind.*;
 import javax.xml.transform.*;
 import javax.xml.transform.stream.*;
 import javax.persistence.*;
 
-@Path("/director")
 public class SolutionDao {
 	
 	EntityManagerFactory factory = Persistence.createEntityManagerFactory("XSLT_JPA");
 	EntityManager em = null;
 	
-	@GET
-	@Path("/{id}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Director findDirector(@PathParam("id") int directorId) {
+	public Director findDirector(int directorId) {
 		Director director = null;
 		
 		em = factory.createEntityManager();
