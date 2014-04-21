c8adcb8507e5469f3836498fa44f42b5954da30d
jannunzi <jannunzi@gmail.com>
2014-04-21 13:24:36 -0400

    Added Organizer class

---
+++ SignUpSheet/src/solution/Organizer.java
@@ -0,0 +1,12 @@
+package solution;
+
+import java.util.Date;
+
+public class Organizer {
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String password;
+	private String email;
+	private Date joined;
+}
