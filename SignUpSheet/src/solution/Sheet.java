ee25ff25343a63663e9811ef2a04bec61029f357
jannunzi <jannunzi@gmail.com>
2014-04-21 13:30:17 -0400

    Added no argument constructor

--- SignUpSheet/src/solution/Sheet.java
+++ SignUpSheet/src/solution/Sheet.java
@@ -1,5 +1,8 @@
 package solution;
 
+import java.util.Date;
+import java.util.List;
+
 public class Sheet {
 	private String name;
 	private String description;
@@ -7,4 +10,43 @@ public class Sheet {
 	private Address where;
 	private Organizer organizer;
 	private List<TimeSlot> timeSlots;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Date getWhen() {
+		return when;
+	}
+	public void setWhen(Date when) {
+		this.when = when;
+	}
+	public Address getWhere() {
+		return where;
+	}
+	public void setWhere(Address where) {
+		this.where = where;
+	}
+	public Organizer getOrganizer() {
+		return organizer;
+	}
+	public void setOrganizer(Organizer organizer) {
+		this.organizer = organizer;
+	}
+	public List<TimeSlot> getTimeSlots() {
+		return timeSlots;
+	}
+	public void setTimeSlots(List<TimeSlot> timeSlots) {
+		this.timeSlots = timeSlots;
+	}
+	public Sheet() {
+		super();
+	}
 }
