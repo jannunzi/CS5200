38c2450eb709d91d693d2faa3be32a80df457805
jannunzi <jannunzi@gmail.com>
2014-04-21 13:28:27 -0400

    Created Sheet class

---
+++ SignUpSheet/src/solution/Sheet.java
@@ -0,0 +1,10 @@
+package solution;
+
+public class Sheet {
+	private String name;
+	private String description;
+	private Date when;
+	private Address where;
+	private Organizer organizer;
+	private List<TimeSlot> timeSlots;
+}
