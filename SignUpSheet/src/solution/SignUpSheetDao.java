87db504c1b455d84286f4d53d4e4d22bd193afbc
jannunzi <jannunzi@gmail.com>
2014-04-21 15:30:35 -0400

    Implement dao.validate()

--- SignUpSheet/src/solution/SignUpSheetDao.java
+++ SignUpSheet/src/solution/SignUpSheetDao.java
@@ -16,9 +16,23 @@ public class SignUpSheetDao {
 		em.getTransaction().commit();
 		em.close();
 	}
+	public boolean validate(int organizer_id) {
+		em = factory.createEntityManager();
+		em.getTransaction().begin();
+
+		Organizer organizer = em.find(Organizer.class, organizer_id);
+
+		em.getTransaction().commit();
+		em.close();
+
+		if(organizer == null)
+			return false;
+		return true;
+	}
 	public static void main(String[] args) {
 		SignUpSheetDao dao = new SignUpSheetDao();
-		
+
+		/*
 		Organizer alice = new Organizer();
 		alice.setFirstName("Alice");
 		alice.setLastName("Wonderland");
@@ -26,5 +40,13 @@ public class SignUpSheetDao {
 		alice.setPassword("queenofhearts");
 		alice.setEmail("alice@wonderland.com");
 		dao.register(alice);
+		*/
+
+		boolean userExists = dao.validate(1);
+		if(userExists)
+			System.out.println("User Exists");
+		else
+			System.out.println("User Does Not Exists");
+
 	}
 }
