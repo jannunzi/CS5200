322f26a8245da9fbb53974368466ec00c525442c
jannunzi <jannunzi@gmail.com>
2014-04-21 13:46:27 -0400

    Configured Address as Entity class

--- SignUpSheet/src/solution/Address.java
+++ SignUpSheet/src/solution/Address.java
@@ -1,6 +1,14 @@
 package solution;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Address {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private String street1;
 	private String street2;
 	private String city;
