d349cab62223875dfa2ea9f32d756c06592065be
jannunzi <jannunzi@gmail.com>
2014-04-21 13:46:54 -0400

    Configured TimeSlot as Entity class

--- SignUpSheet/src/solution/TimeSlot.java
+++ SignUpSheet/src/solution/TimeSlot.java
@@ -2,7 +2,15 @@ package solution;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class TimeSlot {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Date when;
 	private String who;
 	private String notes;
