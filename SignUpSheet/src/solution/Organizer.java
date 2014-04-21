fe90af2ec239073155a9ce09e13a0f55d5e1838e
jannunzi <jannunzi@gmail.com>
2014-04-21 13:26:14 -0400

    Added non argument constructor

--- SignUpSheet/src/solution/Organizer.java
+++ SignUpSheet/src/solution/Organizer.java
@@ -11,4 +11,49 @@ public class Organizer {
 	private String email;
 	private Date joined;
 	private List<Sheet> sheets;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getJoined() {
+		return joined;
+	}
+	public void setJoined(Date joined) {
+		this.joined = joined;
+	}
+	public List<Sheet> getSheets() {
+		return sheets;
+	}
+	public void setSheets(List<Sheet> sheets) {
+		this.sheets = sheets;
+	}
+	public Organizer() {
+		super();
+	}
 }
