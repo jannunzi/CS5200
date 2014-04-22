b4769ab2156f8a9bd26dc55c6e505c3f54e2bea1
jannunzi <jannunzi@gmail.com>
2014-04-21 20:26:12 -0400

    Annotated TimeSlot with XML

--- SignUpSheet/src/solution/TimeSlot.java
+++ SignUpSheet/src/solution/TimeSlot.java
@@ -10,18 +10,27 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 
 @Entity
+@XmlRootElement
 public class TimeSlot {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@XmlAttribute
 	private int id;
 	@Temporal(TemporalType.DATE)
+	@XmlAttribute
 	private Date slotDate;
+	@XmlAttribute
 	private String who;
+	@XmlAttribute
 	private String notes;
 	@ManyToOne
 	@JoinColumn(name="sheet_id")
+	@XmlTransient
 	private Sheet sheet;
 	public int getId() {
 		return id;
