f6bdbae6241ebf0d8ce703aa88c02d7023098592
jannunzi <jannunzi@gmail.com>
2014-10-03 12:35:17 -0400

    Added getConnection() method

---
+++ 2014/fall/eclipse/lectures/OnlineIDE/src/edu/neu/cs5200/onlineide/Application.java
@@ -0,0 +1,33 @@
+package edu.neu.cs5200.onlineide;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Application {
+	public Connection getConnection() {
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			String connectionUrl = "jdbc:mysql://localhost:3306/onlineide";
+			connection = DriverManager.getConnection(connectionUrl, "root", null);
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	public static void main(String[] args) {
+
+	}
+
+}
