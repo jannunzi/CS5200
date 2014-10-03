9b99f7edfec1942cd522bfdc9c1936747baef50e
jannunzi <jannunzi@gmail.com>
2014-10-03 12:31:57 -0400

    Add MySQL Driver

---
+++ 2014/fall/eclipse/lectures/OnlineIDE/src/edu/neu/cs5200/hello/Hello.java
@@ -0,0 +1,13 @@
+package edu.neu.cs5200.hello;
+
+public class Hello {
+
+	public String sayHello(String message) {
+		return "Hello " + message;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
